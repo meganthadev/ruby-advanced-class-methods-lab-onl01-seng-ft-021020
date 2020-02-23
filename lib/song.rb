d0ef7ccb2b82224a5230f1bbe003189ed5b7c7aa
Megan Horsfield <meganthadev@gmail.com>
2020-02-23 21:35:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,10 +37,14 @@ def self.find_by_name(name)
     end
   end 
 
-    def self.find_by_name(name)
+  def self.find_by_name(name)
     @@all.find{|x| x.name == name}
   end
 
+  def self.find_or_create_by_name(name)
+      self.find_by_name(name) || self.create_by_name(name)
+  end
+  
 
   def self.alphabetical
 
