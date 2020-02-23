6595e761907ba77a62a3e130f47a6f792a1d36e2
Megan Horsfield <meganthadev@gmail.com>
2020-02-23 21:33:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,25 @@ class Song
   def save
     self.class.all << self
   end
+  
+   def self.create 
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end  
+
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name
+    song
+  end
+
 
 end
