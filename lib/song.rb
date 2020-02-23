4bba1a1c200a778e2f3c578e84f6009731287840
Megan Horsfield <meganthadev@gmail.com>
2020-02-23 21:34:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,11 +23,12 @@ end
     song
   end
 
-  def self.new_by_name(name)
-    song = self.new 
-    song.name = name
+   def self.create_by_name(name)
+    song = self.create
+    song.name = name 
     song
-  end
+   end 
+
 
 
 end
